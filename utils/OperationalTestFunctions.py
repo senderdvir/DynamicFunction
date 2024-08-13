41647679e7f40452e271d76dcd37e6a919514b9b
senderdvir <senderdvir@gmail.com>
2024-08-13 17:01:44 +0300

    added testing functions

--- utils/OperationalTestFunctions.py
+++ utils/OperationalTestFunctions.py
@@ -9,3 +9,59 @@ def count_records(df, range_between):
 
 def say_hello(name):
     print(name)
+
+    # validate data types - consistency test
+    def validate_data_types(df, expected_types):
+        mismatched_types = {}
+        for column, expected_type in expected_types.items():
+            if not df[column].dtype == expected_type:
+                mismatched_types[column] = (df[column].dtype, expected_type)
+        return bool(mismatched_types), mismatched_types
+
+
+# verifying that the data is updated
+def validate_date_range(df, date_column, start_date, end_date):
+    invalid_dates = df[date_column][(df[date_column] < start_date) | (df[date_column] > end_date)]
+    return (not invalid_dates.empty, invalid_dates.tolist()
+    if not invalid_dates.empty else None)
+
+
+# check if unique columns containing nulls
+def check_nulls(df, column):
+    null_count = df[column].isnull().sum()  # Count nulls in the specified column
+    has_nulls = null_count > 0
+
+    return null_count, has_nulls
+
+
+# check if unique columns containing duplicates
+
+def check_duplicates(df, columns):
+    duplicates = {}
+    has_duplicates = False
+
+    for column in columns:
+        duplicate_rows = df[df.duplicated(subset=[column], keep=False)]
+        duplicate_count = len(duplicate_rows)
+
+        if duplicate_count > 0:
+            duplicates[column] = duplicate_count
+            has_duplicates = True
+
+    return has_duplicates, duplicates
+
+
+# function to verify there are no missing files compared to control file
+
+def check_missing_files(received_df, control_df):
+    # Assume both DataFrames have a 'file_name' column
+    received_files = set(received_df['file_name'])
+    expected_files = set(control_df['file_name'])
+
+    # Determine the missing files
+    missing_files = expected_files - received_files
+
+    # Check if all expected files are received
+    is_matched = len(missing_files) == 0
+
+    return is_matched, list(missing_files) if not is_matched else None
