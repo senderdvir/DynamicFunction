8372db421739ec7235b3f75187f12485db2e69a0
Adi <adiworkr@gmail.com>
2024-08-29 15:21:36 +0300

    Updated write_to_status_table function to filter active tests from operational_table.csv

--- utils/Logger.py
+++ utils/Logger.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-
-def write_to_status_table(file_path: str, data: dict):
+file_path = 'data/status_table.csv'
+def write_to_status_table(data: dict):
     """
     Writes a new row into the status_table CSV file.
 
-    :param file_path: where the status_table.csv file is located
+    :global var 'file_path': where the status_table.csv file is located
     :param data: A dictionary where keys are the column names, and values are the data to be written to the CSV.
     """
     # Convert the dictionary to a DataFrame
     df = pd.DataFrame([data])
-
+    print(f'{data}')
     try:
         # Try to append to the existing file
         df.to_csv(file_path, mode='a', index=False, header=False)
